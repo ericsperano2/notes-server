2b7feafaad22dd52bf25ae8bd91655fad79687ee
Eric Sperano <eric.sperano@gmail.com>
2016-03-04 00:37:37 -0800

    rger

--- server.js
+++ server.js
@@ -94,6 +94,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 //app.use(morgan('dev'));
 
+if (process.env.NOTES_SERVER_ENV !== 'dev') {
+    app.use(function requireHTTPS(req, res, next) {
+        if (!req.secure) {
+            return res.redirect('https://' + req.headers.host + req.url);
+        }
+        next();
+    });
+}
+
 app.use(require('./controllers'));
 
 //app.get('/login/google', passport.authenticate('google', {scope: ['profile']}));
