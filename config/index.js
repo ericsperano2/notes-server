a1a4078194e69c97d72be3ae9122dede506fbcca
Eric Sperano <eric@macrame.local>
2016-03-08 19:23:30 -0800

    statsd

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@
 var Dev = 'dev';
 var Prod = 'prod';
 
+var os = require('os');
 var env = (process.env.NOTES_SERVER_ENV && process.env.NOTES_SERVER_ENV === Dev) ? Dev : Prod;
 var runningInDev = env === Dev;
 var port = process.env.PORT || 3000;
@@ -25,7 +26,11 @@ module.exports = {
         table: 'notes-sessions',
         reapInterval: 600000
     },
-
+    statsd: {
+        host: runningInDev ? '192.168.99.100' : '',
+        port: 8125,
+        prefix: 'notes.' + os.hostname().split('.')[0] + '.'
+    },
     tables: {
         notes: 'notes'
     }
--- models/Note.js
+++ models/Note.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var AWS = require('aws-sdk');
-
 var config = require('../config');
 var logger = require('../logger');
 var docClient = new AWS.DynamoDB.DocumentClient();
+var StatsD = require('node-statsd');
+var statsd = new StatsD.StatsD(config.statsd); // eslint-disable-line new-cap
 
+console.log('statsd', statsd);
 module.exports = {
     getAll: function(userid, callback) {
         logger.debug('Note.getAll for', userid);
@@ -17,31 +19,40 @@ module.exports = {
             }
         };
 
+        statsd.increment('Note.getAll.attempt');
         docClient.query(params, function(err, data) {
             if (err) {
+                statsd.increment('Note.getAll.error');
                 logger.error('getAll', userid, 'Unable to query:\n' + JSON.stringify(err, null, 2));
                 return callback(err);
             }
+            statsd.increment('Note.getAll.success');
             logger.debug('getAll for', userid, ': ' + String(data.Items.length) + ' item(s) found.');
             callback(null, data.Items.reverse());
         });
     },
 
     create: function(note, callback) {
+        statsd.increment('Note.create.attempt');
         logger.debug('Creating note for', note.userid);
         if (!note.userid) {
+            statsd.increment('Note.create.error');
             return callback('must set userid');
         }
-        //note.timestamp = Date.now(); // TODO UTC
+        if (!note.timestamp) {
+            note.timestamp = Date.now(); // TODO UTC
+        }
         var params = {
             TableName: config.tables.notes,
             Item: note
         };
         docClient.put(params, function(err) {
             if (err) {
+                statsd.increment('Note.create.error');
                 logger.error('Unable to add item:', err);
                 callback(err);
             } else {
+                statsd.increment('Note.create.success');
                 logger.debug('Added item:', note);
                 callback(null, note);
             }
@@ -49,6 +60,7 @@ module.exports = {
     },
 
     update: function(note, callback) {
+        statsd.increment('Note.update.attempt');
         logger.debug('Updating note for', note.userid, note.timestamp);
         var params = {
             TableName: config.tables.notes,
@@ -56,15 +68,18 @@ module.exports = {
         };
         docClient.put(params, function(err) {
             if (err) {
+                statsd.increment('Note.update.error');
                 logger.error('Unable to update item:', err);
                 return callback(err);
             }
+            statsd.increment('Note.update.success');
             logger.debug('Updated item:', note);
             callback(null, note);
         });
     },
 
     delete: function(userid, timestamp, callback) {
+        statsd.increment('Note.delete.attempt');
         logger.debug('Deleting note for', userid, timestamp);
         var params = {
             TableName: config.tables.notes,
@@ -75,9 +90,11 @@ module.exports = {
         };
         docClient.delete(params, function(err, data) {
             if (err) {
+                statsd.increment('Note.delete.error');
                 logger.error('Unable to delete item:', err);
                 return callback(err);
             }
+            statsd.increment('Note.delete.success');
             logger.debug('DeleteItem succeeded:', data);
             callback(null, data);
         });
